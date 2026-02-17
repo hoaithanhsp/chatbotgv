07d01ef1b93e4365fe206ac629d3150615cede51
hoaithanhsp <tolyhogroup@gmail.com>
2026-02-18 01:00:11 +0700

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -50,7 +50,7 @@ export const Sidebar: React.FC<SidebarProps> = ({
     };
 
     return (
-        <aside className="w-80 bg-white border-r border-gray-200 flex flex-col h-full hidden md:flex font-sans">
+        <aside className="w-80 bg-white border-r border-gray-200 flex flex-col h-full font-sans">
             {/* Profile Card */}
             <div className="p-4 border-b border-gray-200">
                 <div className="flex items-center gap-3 bg-indigo-50 p-3 rounded-xl">
